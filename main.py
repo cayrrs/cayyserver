3e40481bea6d61f8f3693fef652da0382e07c96e
cayrrs <hodgescayden0@gmail.com>
2026-04-23 19:36:19 -0400

    deploy changes

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# exists
+import subprocess
+
+print("launching server window...")
+
+subprocess.Popen([
+    "konsole",
+    "-e",
+    "bash",
+    "-c",
+    "python3 /home/deck/server/server.py; exec bash"
+])
