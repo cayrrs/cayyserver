0574699b9750fd071540c769b5874e0253333eb9
cayrrs <hodgescayden0@gmail.com>
2026-04-23 19:37:37 -0400

    my bad forgot to stage server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "cayyserver online"
+
+if __name__ == "__main__":
+    print("flask starting...")
+    app.run(host="0.0.0.0", port=8000, debug=True)
