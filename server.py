f92416f095a7fe238ee250bde0eebffe47318d49
cayrrs <hodgescayden0@gmail.com>
2026-04-24 19:24:36 -0400

    remove dc bot temporarily so the updater works

--- server.py
+++ server.py
@@ -9,20 +9,6 @@ app = Flask(__name__)
 start_time = time.time()
 
 
-def start_bot():
-    print("starting bot...")
-
-    proc = subprocess.Popen(
-        ["/home/deck/server/venv/bin/python", "dc bot/main.py"],
-        cwd=".",
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True
-    )
-
-    for line in proc.stdout:
-        print(f"[dcbot] {line}", end="")
-
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -40,10 +26,6 @@ def status():
     })
 
 if __name__ == "__main__":
-    print("starting bot...")
-    time.sleep(5) #delay cause steamdeck dosen't connect to wifi in time
-    start_bot()
-    print("bot started")
     print("flask starting...")
     app.run(host="0.0.0.0", port=8000, debug=False) # i hate that this is a BLOCKING FUCING THING MY GOD
 
